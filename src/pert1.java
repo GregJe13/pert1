9ad038960677f0299266ec569e831f8f5ee21c22
Evan <jegreg04@gmail.com>
2023-08-09 14:13:50 +0700

    init

---
+++ src/pert1.java
@@ -0,0 +1,12 @@
+public class pert1 {
+    public static void main(String[] args){
+        System.out.println("---Data Mahasiswa---");
+        System.out.println("Nama : Evan Jeremia Brianito Santoso");
+        System.out.println("NRP : c14230101");
+        System.out.println("Mata Kuliah : Algoritma dan Pemrograman");
+        System.out.println("Kelas : B");
+        System.out.println("Tahun : 2023/2024");
+        System.out.println("Semester : Gasal");
+        System.out.println("--------------------");
+    }
+}
